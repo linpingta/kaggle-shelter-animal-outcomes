92093438db7cc837c3874f151e8bb1a3386a34a4
chutong <chutong@domob.cn>
2017-01-05 19:43:29 +0800

    update

--- bin/shelter.py
+++ bin/shelter.py
@@ -1,32 +1,38 @@
 #-*- coding: utf-8 -*-
 #!/usr/bin/env python
 # vim: set bg=dark noet ts=4 sw=4 fdm=indent :
-''' simple model caller example'''
+
+""" Shelter animal prediction"""
 __author__ = 'chutong'
 
-import sys, os
+import os
+import sys
 import logging
 import time, datetime
 try:
 	import ConfigParser
-except:
+except ImportError:
 	import configparser as ConfigParser
 basepath = os.path.abspath(os.path.dirname(sys.path[0]))
 libpath = os.path.join(basepath, 'lib')
 sys.path.append(libpath)
+
 from shelter_common_model import ShelterCommonModel
 from external_loader.csv_loader import TsCsvLoader
 from model_loader.xgboost_model import XgboostModel
 from model_loader.randomforest_model import RandomForestModel
+from cleaner.shelter_cleaner import ShelterCleaner
 
 
 if __name__ == '__main__':
 
-	basepath = os.path.abspath(os.path.dirname(sys.path[0]))
-	confpath = os.path.join(basepath, 'conf/shelter.conf')
+	# common cfg
+	confpath = os.path.join(basepath, 'conf/simple.conf')
 	conf = ConfigParser.RawConfigParser()
 	conf.read(confpath)
-	modelconfpath = os.path.join(basepath, 'conf/model_shelter.conf')
+
+	# model parameter cfg
+	modelconfpath = os.path.join(basepath, 'conf/model_simple.conf')
 	model_conf = ConfigParser.RawConfigParser()
 	model_conf.read(modelconfpath)
 
@@ -34,14 +40,15 @@ if __name__ == '__main__':
 		format = '[%(filename)s:%(lineno)s - %(funcName)s %(asctime)s;%(levelname)s] %(message)s',
 		datefmt = '%a, %d %b %Y %H:%M:%S'
 		)
-	logger = logging.getLogger('shelter')
+	logger = logging.getLogger('Shelter')
 
 	now = time.localtime()
 
 	m = ShelterCommonModel(conf)
-	m.set_external_loader(TsCsvLoader(), logger)
-	m.set_model_loader(XgboostModel(model_conf), logger)
-	#m.set_model_loader(RandomForestModel(model_conf), logger)
+	m.external_loader = TsCsvLoader()
+	m.model_loader = XgboostModel(model_conf)
+	#m.model_loader = RandomForestModel(model_conf)
+	m.data_cleaner = ShelterCleaner()
 	try:
 		m.run(now, logger)
 	except Exception as e:
